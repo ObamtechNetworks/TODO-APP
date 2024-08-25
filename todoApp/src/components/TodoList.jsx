98b3a125c500c3c4d25b52d24cac7d7ee54a4222
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-25 19:16:32 +0100

    sorted todos based on completion

--- todoApp/src/components/TodoList.jsx
+++ todoApp/src/components/TodoList.jsx
@@ -2,10 +2,18 @@ import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 
 export default function TodoList({ todos, setTodos }) {
+  // sort the todo list, create a copy, and then convert the booleans to numbers and sort
+  // the sorting is done by the boolean, done and not done, all DONE down and not DONE up
+  const sortedTodos = todos.slice().sort((a, b) => Number(a.done - b.done));
   return (
     <div className={styles.list}>
-      {todos.map((item) => (
-        <TodoItem key={item.name} item={item} todos={todos} setTodos={setTodos} />
+      {sortedTodos.map((item) => (
+        <TodoItem
+          key={item.name}
+          item={item}
+          todos={todos}
+          setTodos={setTodos}
+        />
       ))}
     </div>
   );
